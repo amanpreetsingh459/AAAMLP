26220c1e01b9eb6d43d4818b2c11903afb9829a7
Amanpreet Singh <amanpreetsingh459@gmail.com>
2021-03-16 16:26:39 +0530

    Add confusion-matrix implementation

--- chapter4_Evaluation-metrics/accuracies.py
+++ chapter4_Evaluation-metrics/accuracies.py
@@ -274,5 +274,22 @@ print("Multi-class precision with weighted average:", metrics.precision_score(y_
 
 print("Multi-class f1-score with weighted average:", metrics.f1_score(y_true, y_pred, average="weighted"))
 
-
+# ###confusion matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import metrics
+# some targets
+y_true = [0, 1, 2, 0, 1, 2, 0, 2, 2]
+#some predictions
+y_pred = [0, 2, 1, 0, 2, 1, 0, 0, 2]
+# get confusion matrix from sklearn
+cm = metrics.confusion_matrix(y_true, y_pred)
+# plot using matplotlib and seaborn
+plt.figure(figsize=(10, 10))
+cmap = sns.cubehelix_palette(50, hue=0.05, rot=0, light=0.9, dark=0,
+as_cmap=True)
+sns.set(font_scale=2.5)
+sns.heatmap(cm, annot=True, cmap=cmap, cbar=False)
+plt.ylabel('Actual Labels', fontsize=20)
+plt.xlabel('Predicted Labels', fontsize=20)
 
