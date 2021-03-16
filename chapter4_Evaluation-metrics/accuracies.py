98248e0a0c7a80c089a2d42b8437b5b4595337df
Amanpreet Singh <amanpreetsingh459@gmail.com>
2021-03-16 13:31:35 +0530

    Add precision, recall, f1-score implementation

--- chapter4_Evaluation-metrics/accuracies.py
+++ chapter4_Evaluation-metrics/accuracies.py
@@ -25,13 +25,13 @@ def accuracy(y_true, y_pred):
     # which is correct predictions over the number of samples
     return correct_counter / len(y_true)
 
-print("Accuracy V1:", accuracy(l1, l2))
+print("Accuracy V1:", accuracy(l1, l2))      #0.625
 
 
 # ## accuracy using scikit-learn
 
 from sklearn import metrics
-print("Accuracy using sklearn:", metrics.accuracy_score(l1, l2))
+print("Accuracy using sklearn:", metrics.accuracy_score(l1, l2))      #0.625
 
 
 # ## accuracy calculation with True positive, True negative, False positive, False negative
@@ -95,13 +95,13 @@ def false_negative(y_true, y_pred):
     return fn
 
 
-print("Number of true positive:", true_positive(l1, l2))
+print("Number of true positive:", true_positive(l1, l2))      #0.625
 
-print("Number of false positive:", false_positive(l1, l2))
+print("Number of false positive:", false_positive(l1, l2))      #0.625
 
-print("Number of false negative:", false_negative(l1, l2))
+print("Number of false negative:", false_negative(l1, l2))      #0.625
 
-print("Number of true negative:", true_negative(l1, l2))
+print("Number of true negative:", true_negative(l1, l2))      #0.625
 
 # ### Accuracy Score = (TP + TN) / (TP + TN + FP + FN)
 
@@ -120,14 +120,64 @@ def accuracy_v2(y_true, y_pred):
     return accuracy_score
 
 
-print("Accuracy V2:", accuracy_v2(l1, l2))
+print("Accuracy V2:", accuracy_v2(l1, l2))      #0.625
 
-print("Accuracy V1:", accuracy(l1, l2))
+print("Accuracy V1:", accuracy(l1, l2))      #0.625
 
-print("Accuracy using sklearn:", metrics.accuracy_score(l1, l2))
+print("Accuracy using sklearn:", metrics.accuracy_score(l1, l2))      #0.625
+
+# ###Precision = TP / (TP + FP)
+
+def precision(y_true, y_pred):
+    """
+    Function to calculate precision
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: precision score
+    """
+    tp = true_positive(y_true, y_pred)
+    fp = false_positive(y_true, y_pred)
+    precision = tp / (tp + fp)
+    return precision
+    # ## Bingo!!!
+
+print("Precision:", precision(l1, l2))      #0.6666666666666666
+
+# ###Recall = TP / (TP + FN)
+
+def recall(y_true, y_pred):
+    """
+    Function to calculate recall
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: recall score
+    """
+    tp = true_positive(y_true, y_pred)
+    fn = false_negative(y_true, y_pred)
+    recall = tp / (tp + fn)
+    return recall
+
+print("Recall:", recall(l1, l2))      #0.5
 
 
-# ## Bingo!!!
+# ###F1 = 2PR / (P + R)
+# ###F1 = 2TP / (2TP + FP + FN)
 
+def f1(y_true, y_pred):
+    """
+    Function to calculate f1 score
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: f1 score
+    """
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    score = 2 * p * r / (p + r)
+    return score
+
+print("F1 score:", f1(l1, l2))      #0.5714285714285715
+
+from sklearn import metrics
+print("F1 score through sklearn:", metrics.f1_score(l1, l2))      #0.5714285714285715
 
 
