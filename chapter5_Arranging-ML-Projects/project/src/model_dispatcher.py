eb92d00e9386ddd0305f85286e8c8b914f26b247
Amanpreet Singh <amanpreetsingh459@gmail.com>
2021-03-17 16:53:02 +0530

    Add one more model in model_dispatcher.py

--- chapter5_Arranging-ML-Projects/project/src/model_dispatcher.py
+++ chapter5_Arranging-ML-Projects/project/src/model_dispatcher.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from sklearn import tree
+from sklearn import ensemble
 
 models = {
     "decision_tree_gini": tree.DecisionTreeClassifier(criterion="gini"),    
     "decision_tree_entropy": tree.DecisionTreeClassifier(criterion="entropy"),
+    "rf": ensemble.RandomForestClassifier(),
 }
